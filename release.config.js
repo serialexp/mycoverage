986ceba793693fb99da92b5bbaf0725a0233796d
Bart Riepe <bart.riepe@asurion.com>
2022-07-04 18:46:50 +0900

    fix: make sure package.json is updated too

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     "@semantic-release/changelog",
     "@semantic-release/github",
     [
@@ -23,6 +24,7 @@ module.exports = {
       "@semantic-release/git",
       {
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json", "npm-shrinkwrap.json"],
       },
     ],
   ],
