b1e1f9e134c15d5d8465f65913bb09f9cb33c8e4
Bart Riepe <bart.riepe@asurion.com>
2022-07-04 16:07:38 +0900

    fix: push to github when release is made

--- release.config.js
+++ release.config.js
@@ -19,5 +19,11 @@ module.exports = {
         },
       },
     ],
+    [
+      "@semantic-release/git",
+      {
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 }
