94d73aa8da6980ccaeb09b3f77dd3428d2003d6e
Bart Riepe <bart.riepe@asurion.com>
2023-05-29 12:33:21 +0900

    fix: set log level to warn on production

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,19 +1,20 @@
-import {
-	AuthServerPlugin,
-	PrismaStorage,
-	simpleRolesIsAuthorized,
-} from "@blitzjs/auth";
-import { setupBlitzServer } from "@blitzjs/next";
-import db from "db";
+import { AuthServerPlugin, PrismaStorage, simpleRolesIsAuthorized } from "@blitzjs/auth"
+import { setupBlitzServer } from "@blitzjs/next"
+import { BlitzLogger } from "blitz"
+import db from "db"
 
-import { authConfig } from "./blitz-client";
+import { authConfig } from "./blitz-client"
 
 export const { gSSP, gSP, api } = setupBlitzServer({
-	plugins: [
-		AuthServerPlugin({
-			...authConfig,
-			storage: PrismaStorage(db),
-			isAuthorized: simpleRolesIsAuthorized,
-		}),
-	],
-});
+  plugins: [
+    AuthServerPlugin({
+      ...authConfig,
+      storage: PrismaStorage(db),
+      isAuthorized: simpleRolesIsAuthorized,
+    }),
+  ],
+  logger: BlitzLogger({
+    // no more noise in production
+    minLevel: process.env.NODE_ENV === "production" ? "warn" : "info",
+  }),
+})
