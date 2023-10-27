3bd960126e6fe330573c50dfc72d37c799578fa8
Bart Riepe <bart@serial-experiments.com>
2023-10-27 16:04:26 +0900

    feat: add endpoint for retrieving coverage summary from pr ids

--- src/coverage/queries/getLogs.ts
+++ src/coverage/queries/getLogs.ts
@@ -1,46 +1,46 @@
-import { Ctx } from "blitz";
-import db from "db";
+import { Ctx } from "blitz"
+import db, { Prisma } from "db"
 
 export default async function getLogs(
-	args: { filter?: string; minDate: Date; maxDate: Date; commitRef?: string },
-	{ session }: Ctx,
+  args: { filter?: string; minDate: Date; maxDate: Date; commitRef?: string },
+  { session }: Ctx
 ) {
-	const conditions = {};
+  const conditions: Prisma.JobLogWhereInput = {}
 
-	if (args.filter) {
-		conditions.OR = [
-			{
-				message: {
-					contains: args.filter,
-				},
-			},
-			{
-				status: {
-					contains: args.filter,
-				},
-			},
-			{
-				name: {
-					contains: args.filter,
-				},
-			},
-		];
-	}
-	if (args.minDate || args.maxDate) {
-		conditions.createdDate = {
-			gte: args.minDate ?? undefined,
-			lte: args.maxDate ?? undefined,
-		};
-	}
-	if (args.commitRef) {
-		conditions.commitRef = args.commitRef;
-	}
+  if (args.filter) {
+    conditions.OR = [
+      {
+        message: {
+          contains: args.filter,
+        },
+      },
+      {
+        status: {
+          contains: args.filter,
+        },
+      },
+      {
+        name: {
+          contains: args.filter,
+        },
+      },
+    ]
+  }
+  if (args.minDate || args.maxDate) {
+    conditions.createdDate = {
+      gte: args.minDate ?? undefined,
+      lte: args.maxDate ?? undefined,
+    }
+  }
+  if (args.commitRef) {
+    conditions.commitRef = args.commitRef
+  }
 
-	return db.jobLog.findMany({
-		orderBy: {
-			createdDate: "desc",
-		},
-		where: conditions,
-		take: 100,
-	});
+  return db.jobLog.findMany({
+    orderBy: {
+      createdDate: "desc",
+    },
+    where: conditions,
+    take: 100,
+  })
 }
